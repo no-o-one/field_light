b0b4634485c5a9582f08cac8874e84bdde82a596
jinxular <jnorvell@depaul.edu>
2026-07-08 16:40:31 -0500

    not much

--- esp_wifi_lightstrip/esp_osc/esp_osc_main/osc_sender.py
+++ esp_wifi_lightstrip/esp_osc/esp_osc_main/osc_sender.py
@@ -7,7 +7,7 @@ from pythonosc import udp_client
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("--ip", default="192.168.0.255",
+  parser.add_argument("--ip", default="192.168.1.255",
       help="The ip of the OSC server")
   parser.add_argument("--port", type=int, default=6000,
       help="The port the OSC server is listening on")
@@ -15,11 +15,16 @@ if __name__ == "__main__":
 
   client = udp_client.SimpleUDPClient(args.ip, args.port, 10)
 
-  for x in range(1):
-    time.sleep(0)
+  while(True):
+    time.sleep(0.2)
+    client.send_message("/strip/preset/1", 0)
+    print("sent1")
+  
+    time.sleep(0.2)
     client.send_message("/strip/preset/0", 0)
-    print("sent")
+    print("sent0")
    
+
     # client.send_message("/strip/preset/1", 0)
     # print("sent")
     # time.sleep(5)
---
+++ readme.md
@@ -0,0 +1,6 @@
+##field building lighting project
+
+this repo is mostly focused on an ArtNet and OSC based control network for DePaul's IRL field building immersive.
+
+
+Once we open there might be made a nice library to showcase the work done here.
